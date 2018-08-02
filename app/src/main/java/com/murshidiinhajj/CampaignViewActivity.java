3287f0ef0ee9b745a97cff660b1efaba667c9e88
Somaru-chan <42027331+Somaru-chan@users.noreply.github.com>
2018-08-02 17:39:02 +0300

    Committing changes to UI elements (Toolbar and Icons)

--- app/src/main/java/com/murshidiinhajj/CampaignViewActivity.java
+++ app/src/main/java/com/murshidiinhajj/CampaignViewActivity.java
@@ -17,14 +17,14 @@ public class CampaignViewActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_main_points:
+                    mTextMessage.setText(R.string.title_main_points);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_campaigns:
+                    mTextMessage.setText(R.string.title_campaigns);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_track_guide:
+                    mTextMessage.setText(R.string.title_track_guide);
                     return true;
             }
             return false;
